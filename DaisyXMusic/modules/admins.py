0d47bc7527b2c604051949687b1477fc1b129030
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-08-25 14:52:44 +0100

    Update admins.py

--- DaisyXMusic/modules/admins.py
+++ DaisyXMusic/modules/admins.py
@@ -14,76 +14,44 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# Modified by @InukaAsith
-
-# Added /auth and /deauth by azimazizov9150 <https://github.com/azimazizov9150>
 
 from asyncio import QueueEmpty
 from DaisyXMusic.config import que
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from DaisyXMusic.function.admins import admins
+from DaisyXMusic.function.admins import set
 from DaisyXMusic.helpers.channelmusic import get_chat_id
 from DaisyXMusic.helpers.decorators import authorized_users_only, errors
 from DaisyXMusic.helpers.filters import command, other_filters
 from DaisyXMusic.services.callsmusic import callsmusic
 from DaisyXMusic.services.queues import queues
 
-# By azimazizov9150 <https://github.com/azimazizov9150>
 
-@Client.on_message(filters.command("admincache"))
-async def update_admin(client, message):
-    global admins
-    new_admins = []
-    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
-    for u in new_ads:
-        new_admins.append(u.user.id)
-    admins[message.chat.id] = new_admins
+@Client.on_message(filters.command("adminreset"))
+async def update_admin(client, message: Message):
+    chat_id = get_chat_id(message.chat)
+    set(
+        chat_id,
+        [
+            member.user
+            for member in await message.chat.get_members(filter="administrators")
+        ],
+    )
     await message.reply_text("❇️ Admin cache refreshed!")
 
 
-@Client.on_message(filters.command("auth"))
-@authorized_users_only
-async def authenticate(client, message):
-    global admins
-    if not message.reply_to_message:
-        await message.reply("reply to message to authorize user!")
-        return
-    if message.reply_to_message.from_user.id not in admins[message.chat.id]:
-        new_admins = admins[message.chat.id]
-        new_admins.append(message.reply_to_message.from_user.id)
-        admins[message.chat.id] = new_admins
-        await message.reply("user authorized.")
-    else:
-        await message.reply("user already authorized!")
-
-
-@Client.on_message(filters.command("deauth"))
-@authorized_users_only
-async def deautenticate(client, message):
-    global admins
-    if not message.reply_to_message:
-        await message.reply("reply to message to deauthorize user!")
-        return
-    if message.reply_to_message.from_user.id in admins[message.chat.id]:
-        new_admins = admins[message.chat.id]
-        new_admins.remove(message.reply_to_message.from_user.id)
-        admins[message.chat.id] = new_admins
-        await message.reply("user deauthorized")
-    else:
-        await message.reply("user already deauthorized!")
-
-        
 @Client.on_message(command("pause") & other_filters)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    if chat_id in callsmusic.pytgcalls.active_calls:
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "paused"
+    ):
         await message.reply_text("❗ Nothing is playing!")
     else:
-        await callsmusic.pytgcalls.pause_stream(chat_id)
+        callsmusic.pytgcalls.pause_stream(chat_id)
         await message.reply_text("▶️ Paused!")
 
 
@@ -92,10 +60,12 @@ async def pause(_, message: Message):
 @authorized_users_only
 async def resume(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    if chat_id in callsmusic.pytgcalls.active_calls:
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "playing"
+    ):
         await message.reply_text("❗ Nothing is paused!")
     else:
-        await callsmusic.pytgcalls.resume_stream(chat_id)
+        callsmusic.pytgcalls.resume_stream(chat_id)
         await message.reply_text("⏸ Resumed!")
 
 
@@ -104,7 +74,7 @@ async def resume(_, message: Message):
 @authorized_users_only
 async def stop(_, message: Message):
     chat_id = get_chat_id(message.chat)
-    if chat_id in callsmusic.pytgcalls.active_calls:
+    if chat_id not in callsmusic.pytgcalls.active_calls:
         await message.reply_text("❗ Nothing is streaming!")
     else:
         try:
@@ -112,7 +82,7 @@ async def stop(_, message: Message):
         except QueueEmpty:
             pass
 
-        await callsmusic.pytgcalls.leave_group_call(chat_id)
+        callsmusic.pytgcalls.leave_group_call(chat_id)
         await message.reply_text("❌ Stopped streaming!")
 
 
@@ -122,15 +92,15 @@ async def stop(_, message: Message):
 async def skip(_, message: Message):
     global que
     chat_id = get_chat_id(message.chat)
-    if chat_id in callsmusic.pytgcalls.active_calls:
+    if chat_id not in callsmusic.pytgcalls.active_calls:
         await message.reply_text("❗ Nothing is playing to skip!")
     else:
         queues.task_done(chat_id)
 
         if queues.is_empty(chat_id):
-            await callsmusic.pytgcalls.leave_group_call(chat_id)
+            callsmusic.pytgcalls.leave_group_call(chat_id)
         else:
-            await callsmusic.pytgcalls.change_stream(
+            callsmusic.pytgcalls.change_stream(
                 chat_id, queues.get(chat_id)["file"]
             )
 
@@ -140,3 +110,16 @@ async def skip(_, message: Message):
     if not qeue:
         return
     await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+
+
+@Client.on_message(filters.command("admincache"))
+@errors
+async def admincache(client, message: Message):
+    set(
+        message.chat.id,
+        [
+            member.user
+            for member in await message.chat.get_members(filter="administrators")
+        ],
+    )
+    await message.reply_text("❇️ Admin cache refreshed!")
